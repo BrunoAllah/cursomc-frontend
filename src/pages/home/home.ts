6ae7962d6b937624e88034b536e01eb58974146d
Bruno Araújo <brunooliveiraaraujo@hotmail.com>
2020-08-05 17:10:08 -0300

    Lazy loading e tela de login e categorias

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
 
+  }
+
+  ionViewWillEnter() {
+    this.menuCtrl.swipeEnable(false);
+  }
+
+  ionViewDidLeave() {
+    this.menuCtrl.swipeEnable(true);
+  }
+
+  login() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
